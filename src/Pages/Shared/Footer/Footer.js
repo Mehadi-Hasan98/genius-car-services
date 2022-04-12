cd16316201cc26d1fe9d45ebaed385ecb49cdabc
Mehadi Hasan <Mehadihasan.phy@gmail.com>
2022-04-12 23:00:58 +0600

    Header and Footer done

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -4,8 +4,8 @@ const Footer = () => {
     const today = new Date();
     const year = today.getFullYear();
     return (
-        <footer className='text-center mt-5'>
-            <p><small>Copyright © {year}</small></p>
+        <footer className='text-center mt-5 border border-primary pt-4 pb-4 bg-primary'>
+            <p><small className='fw-bold'>Copyright © {year}</small> By Genius Car Services.</p>
         </footer>
     );
 };
